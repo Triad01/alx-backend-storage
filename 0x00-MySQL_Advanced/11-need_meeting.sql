887d3f99fbaa8e283692320ccb8c59e93897473e
triad01 <luckypee01@gmail.com>
2024-08-10 12:05:17 +0100

    corrected task

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,23 +1,5 @@
--- Creates a view need_meeting that lists all students that have a score
--- under 80 (strict) and no last_meeting or more than 1 month.
+-- Creates a view for students under 80 since last months meeting
 DROP VIEW IF EXISTS need_meeting;
-CREATE VIEW need_meeting AS
-    SELECT name
-        FROM students
-        WHERE score < 80 AND
-            (
-                last_meeting IS NULL
-                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
-            )
--- Creates a view need_meeting that lists all students that have a score
--- under 80 (strict) and no last_meeting or more than 1 month.
-DROP VIEW IF EXISTS need_meeting;
-CREATE VIEW need_meeting AS
-    SELECT name
-        FROM students
-        WHERE score < 80 AND
-            (
-                last_meeting IS NULL
-                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
-            )
-;;
+CREATE VIEW need_meeting AS SELECT name FROM students
+WHERE score < 80 AND 
+    (last_meeting IS NULL OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH));
