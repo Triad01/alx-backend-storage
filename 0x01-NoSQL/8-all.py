62c9a6ebfbd5ab3703e67f0b830d084e07c188e0
triad01 <luckypee01@gmail.com>
2024-08-12 20:52:39 +0100

    task 8 corrected

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+'''Lists all documents in a collection
+'''
 
 
 def list_all(mongo_collection):
-    """
-        lists all documents in a collection
-    """
+    '''Lists all documents in a collection.
+    '''
     return [doc for doc in mongo_collection.find()]
